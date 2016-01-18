002e5ff3b8651353dde7a965f2a0641287def5fc
aki <wiki908.zh@gmail.com>
2016-01-18 22:35:55 +0800

    添加指令如 '=2'，复制对应序号文本（支持全角）

--- LabelPlus/LabelFileManager.cs
+++ LabelPlus/LabelFileManager.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * 
+ *
  * Copyright 2015, Noodlefighter
  * Released under GPL License.
  *
@@ -7,33 +7,38 @@
  */
 
 #region Using Directives
+
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
-#endregion
+using System.Text.RegularExpressions;
+
+#endregion Using Directives
 
 namespace LabelPlus
 {
     public class LabelFileManager
     {
         #region Const
+
         // 主次版本号
         // 主版本迭代一次 说明旧版本对新版本不兼容 可能无法正常读取 或导致丢失信息
         // 次版本迭代一次 说明文件结构有变 但旧版本可以读取不支持某些特性 不会导致信息丢失
-        const int MY_FILE_VER_FIRST = 1;
-        const int MY_FILE_VER_LAST = 0;
+        private const int MY_FILE_VER_FIRST = 1;
+
+        private const int MY_FILE_VER_LAST = 0;
 
         // 文件头成员数
-        const int FILEHEAD_LENGHT = 2;
+        private const int FILEHEAD_LENGHT = 2;
 
         //
-        static readonly string[] FILEHEAD_DEFAULT = { 
-            MY_FILE_VER_FIRST.ToString(), 
+        private static readonly string[] FILEHEAD_DEFAULT = {
+            MY_FILE_VER_FIRST.ToString(),
             MY_FILE_VER_LAST.ToString() };
 
-        #endregion
+        #endregion Const
 
         internal enum stateEnum
         {
@@ -41,12 +46,14 @@ namespace LabelPlus
             file,   //文件区块
             label,  //标签区块
         }
+
         internal enum strlineType
         {
             normal,     //普通文本行
             fileHead,   //文件文本行
-            labelHead,  //标签文本行            
+            labelHead,  //标签文本行
         }
+
         internal struct getStrlineTypeResult
         {
             public strlineType type;
@@ -55,24 +62,26 @@ namespace LabelPlus
 
         #region Fields
 
-        string[] fileHead;   //文件头 0:主版本号 1:次版本号
-        List<string> groupStringList; //分组名称定义
-        string comment; //用户注释
+        private string[] fileHead;   //文件头 0:主版本号 1:次版本号
+        private List<string> groupStringList; //分组名称定义
+        private string comment; //用户注释
 
         //标签信息
-        Dictionary<string, List<LabelItem>> store;        
-        
-        #endregion
+        private Dictionary<string, List<LabelItem>> store;
+
+        #endregion Fields
 
         #region Constructors
-        public LabelFileManager(){
+
+        public LabelFileManager()
+        {
             fileHead = FILEHEAD_DEFAULT;
             groupStringList = new List<string>();
             comment = GlobalVar.DefaultComment;
             store = new Dictionary<string, List<LabelItem>>();
-
         }
-        #endregion
+
+        #endregion Constructors
 
         #region Events
 
@@ -85,19 +94,23 @@ namespace LabelPlus
         {
             if (FileListChanged != null) FileListChanged(this, new EventArgs());
         }
+
         internal void OnLabelItemListChanged()
         {
             if (LabelItemListChanged != null) LabelItemListChanged(this, new EventArgs());
         }
+
         internal void OnLabelItemTextChanged()
         {
             if (LabelItemTextChanged != null) LabelItemTextChanged(this, new EventArgs());
         }
-        internal void OnGroupListChanged() 
+
+        internal void OnGroupListChanged()
         {
             if (GroupListChanged != null) GroupListChanged(this, new EventArgs());
         }
-        #endregion
+
+        #endregion Events
 
         #region Properties
 
@@ -110,6 +123,7 @@ namespace LabelPlus
                 return store[file];
             }
         }
+
         public LabelItem this[string file, int index]
         {
             get
@@ -122,21 +136,26 @@ namespace LabelPlus
             }
         }
 
-        public List<string> GroupList { 
+        public List<string> GroupList
+        {
             get { return groupStringList; }
-            set {
+            set
+            {
                 groupStringList = value;
                 OnGroupListChanged();
             }
         }
-        public string Comment { 
+
+        public string Comment
+        {
             get { return comment; }
-            set {
+            set
+            {
                 comment = value;
             }
         }
 
-        #endregion
+        #endregion Properties
 
         #region Methods
 
@@ -146,8 +165,8 @@ namespace LabelPlus
         private void readLabelFileStartBlocks(string nowText)
         {
             //老版本 无start blocks 特殊处理
-            if (nowText.Trim() == "") { 
-
+            if (nowText.Trim() == "")
+            {
                 //block1
                 for (int i = 0; i < FILEHEAD_LENGHT; i++)
                 {
@@ -167,7 +186,7 @@ namespace LabelPlus
                         break;
                     }
                 }
-                
+
                 //block end
                 comment = GlobalVar.DefaultComment;
 
@@ -181,22 +200,24 @@ namespace LabelPlus
             string[] tmp;
             //区块1 文件头
             tmp = textBlocks[0].Split(',');
-            for (int i = 0; i < FILEHEAD_LENGHT; i++) {
-                if(i<tmp.Length)
+            for (int i = 0; i < FILEHEAD_LENGHT; i++)
+            {
+                if (i < tmp.Length)
                     fileHead[i] = tmp[i].Trim();               //实际值
                 else
                     fileHead[i] = FILEHEAD_DEFAULT[i];      //默认值
             }
-            
+
             //检查版本信息
             if (Convert.ToInt16(fileHead[0]) > MY_FILE_VER_FIRST)
                 throw new Exception(StringResources.GetValue("error_file_version_over"));
 
             //区块2 分组信息
             tmp = textBlocks[1].Trim().Split('\r');
-            foreach (string str in tmp) {
+            foreach (string str in tmp)
+            {
                 string t = str.Trim();
-                if(t!="")
+                if (t != "")
                     groupStringList.Add(t);
             }
 
@@ -212,7 +233,8 @@ namespace LabelPlus
             string result = "";
 
             //区块1 文件头
-            foreach (string str in fileHead) {
+            foreach (string str in fileHead)
+            {
                 result += str + ",";
             }
             result = result.Substring(0, result.Length - 1);    //去掉最后一个逗号
@@ -231,7 +253,8 @@ namespace LabelPlus
             return result;
         }
 
-        internal bool addFile(string file) {
+        internal bool addFile(string file)
+        {
             try
             {
                 store.Add(file, new List<LabelItem>());
@@ -239,7 +262,8 @@ namespace LabelPlus
             }
             catch { return false; }
         }
-        internal bool addLabelItem(string file, LabelItem item, int insertIndex = -1) 
+
+        internal bool addLabelItem(string file, LabelItem item, int insertIndex = -1)
         {
             try
             {
@@ -266,6 +290,7 @@ namespace LabelPlus
             }
             catch { return false; }
         }
+
         public bool AddLabelItem(string file, LabelItem item, int insertIndex = -1)
         {
             try
@@ -279,6 +304,7 @@ namespace LabelPlus
             }
             catch { return false; }
         }
+
         public bool UpdateLabelItemText(string file, int index, string text)
         {
             try
@@ -312,6 +338,7 @@ namespace LabelPlus
             }
             catch { return false; }
         }
+
         //public bool DelAllFiles()
         //{
         //    try
@@ -324,7 +351,7 @@ namespace LabelPlus
         //    catch { return false; }
         //}
 
-        public bool NewLabelFile(string[] groups) 
+        public bool NewLabelFile(string[] groups)
         {
             fileHead = FILEHEAD_DEFAULT;
             comment = GlobalVar.DefaultComment;
@@ -347,6 +374,7 @@ namespace LabelPlus
             }
             catch { return false; }
         }
+
         public bool DelAllLabelInFile(string file)
         {
             try
@@ -402,13 +430,13 @@ namespace LabelPlus
                                 nowText += "\r\n" + result.value[0];
                             }
                             break;
+
                         case stateEnum.file:
                             if (result.type == strlineType.labelHead)
                             {
                                 state = stateEnum.label;
                                 nowText = "";
                                 nowLabelResultValues = result.value;
-
                             }
                             else if (result.type == strlineType.fileHead)
                             {
@@ -418,6 +446,7 @@ namespace LabelPlus
                                 if (!addFilenameToStore(nowFilename)) state = stateEnum.start;
                             }
                             break;
+
                         case stateEnum.label:
                             switch (result.type)
                             {
@@ -425,12 +454,14 @@ namespace LabelPlus
                                     if (nowText == "") nowText = result.value[0];
                                     else nowText += "\r\n" + result.value[0];
                                     break;
+
                                 case strlineType.labelHead:
                                     //保存之前的内容
                                     addLabelToStore(nowText, nowLabelResultValues, nowFilename);
                                     nowText = "";
                                     nowLabelResultValues = result.value;
                                     break;
+
                                 case strlineType.fileHead:
                                     //保存之前的内容
                                     addLabelToStore(nowText, nowLabelResultValues, nowFilename);
@@ -441,7 +472,6 @@ namespace LabelPlus
                                     break;
                             }
                             break;
-
                     }   //switch (state)
                 }   //while (!sr.EndOfStream)
 
@@ -463,7 +493,7 @@ namespace LabelPlus
                 OnLabelItemListChanged();
                 OnGroupListChanged();
             }
-            catch(Exception e) 
+            catch (Exception e)
             {
                 throw new Exception("ReadFromFileError in line" + error_lineNum.ToString()
                     + "\r\n" + error_state
@@ -471,74 +501,115 @@ namespace LabelPlus
             }
         }
 
+        private void replaceText()
+        {
+            try
+            {
+                // foreach 是只读的 _(:з」∠)_
+                store = store.Select(
+                    x =>
+                {
+                    var tlist = x.Value;
+
+                    bool changed;
+                    do
+                    {
+                        changed = false;
+                        for (int i = 0; i < tlist.Count; ++i)
+                        {
+                            if (Regex.IsMatch(tlist[i].Text, @"=|＝"))
+                            {
+                                try
+                                {
+                                    var idx_raw = tlist[i].Text.Substring(1);
+                                    string idx_t = string.Concat(
+                                        idx_raw
+                                        .ToCharArray()
+                                        .Select(y => { if (y >= '０' && y <= '９') y ^= '\xFF20'; return y + ""; })
+                                        .ToArray()
+                                        );
+                                    int idx = int.Parse(idx_t);
+                                    if (i != idx - 1)
+                                    {
+                                        tlist[i].Text = tlist[idx - 1].Text;
+                                        changed = true;
+                                    }
+                                }
+                                catch { }
+                            }
+                        }
+                    } while (changed == true);
+
+                    return new KeyValuePair<string, List<LabelItem>>(x.Key, tlist);
+                }
+                ).ToDictionary(x => x.Key, x => x.Value);
+            }
+            catch { }
+        }
+
         public bool ToFile(string path)
         {
             try
             {
-                FileStream fs = new FileStream(path, FileMode.Create);
-                StreamWriter sr = new StreamWriter(fs, Encoding.UTF8);
+                replaceText();
 
-                var filenames = store.Keys;
+                var sb = new StringBuilder();
+                sb.AppendLine(getLabelFileStartBlocksString());
 
-                sr.WriteLine(getLabelFileStartBlocksString());
-
-                foreach (var name in filenames)
+                foreach (var file in store.Keys)
                 {
                     int count = 0;
-                    List<LabelItem> items = store[name];
+                    List<LabelItem> items = store[file];
 
-                    sr.WriteLine();
-                    sr.WriteLine(">>>>>>>>[" + name + "]<<<<<<<<");
+                    sb.AppendLine();
+                    sb.AppendLine(">>>>>>>>[" + file + "]<<<<<<<<");
                     foreach (var n in items)
                     {
                         count++;
-                        sr.WriteLine("----------------[" + count.ToString() +
-                            "]----------------[" + 
-                            n.X_percent.ToString("F3") + "," + 
+                        sb.AppendLine("----------------[" + count.ToString() +
+                            "]----------------[" +
+                            n.X_percent.ToString("F3") + "," +
                             n.Y_percent.ToString("F3") + "," +
-                            n.Category.ToString() + 
+                            n.Category.ToString() +
                             "]");
-                        sr.WriteLine(n.Text);
-                        sr.WriteLine();
+                        sb.AppendLine(n.Text);
+                        sb.AppendLine();
                     }
                 }
-                sr.Close();
-                fs.Close();
+
+                var sr = new StreamWriter(path, false, Encoding.UTF8);
+                sr.Write(sb.ToString());
+                sr.Dispose();
 
                 return true;
             }
             catch { return false; }
-
         }
+
         internal getStrlineTypeResult getStrlineType(string str)
         {
             str = str.Trim();
+            string ptrn_imgfn = @"^>+\[(?<img>.*)\]<+$";
+            string ptrn_line = @"^-+\[(?<idx>.*)\]-+(\[(?<x>[\d\.]*)\,(?<y>[\d\.]*)(\,(?<g>\d*))?\])?$";
 
             getStrlineTypeResult tmp = new getStrlineTypeResult();
-            if (str.StartsWith(">>>>>>>>[") && str.IndexOf("]<<<<<<<<") > 9)
+            if (Regex.IsMatch(str, ptrn_imgfn))
             {
                 tmp.type = strlineType.fileHead;
                 tmp.value = new string[1];
-                tmp.value[0] = str.Substring(9, str.IndexOf("]<<<<<<<<") - 9);
+
+                tmp.value[0] = Regex.Match(str, ptrn_imgfn).Groups["img"].Value;
             }
-            else if (str.StartsWith("----------------[") && str.IndexOf("]----------------") > 17)
+            else if (Regex.IsMatch(str, ptrn_line))
             {
                 tmp.type = strlineType.labelHead;
+                var g = Regex.Match(str, ptrn_line).Groups;
                 List<string> tmpList = new List<string>();
-                tmpList.Add(str.Substring(17, str.IndexOf("]----------------") - 17));
-                string rightText = str.Substring(str.IndexOf("]----------------") + 17);
-                if (rightText.StartsWith("[") && rightText.EndsWith("]"))
-                {
-                    rightText = rightText.Substring(1, rightText.Length - 2);
-                    string[] splitText = rightText.Split(',');
-                    foreach (string s in splitText) { tmpList.Add(s); }
-                }
-                else
-                {
-                    tmp.value = tmpList.ToArray();
-                    goto TheEnd;
-                }
-                tmp.value = tmpList.ToArray();
+                tmpList.Add(g["idx"].Value);
+                tmpList.Add(g["x"].Value);
+                tmpList.Add(g["y"].Value);
+                tmpList.Add(g["g"].Value);
+                tmp.value = tmpList.Where(x => !string.IsNullOrEmpty(x)).ToArray();
             }
             else
             {
@@ -546,27 +617,25 @@ namespace LabelPlus
                 tmp.value = new string[1];
                 tmp.value[0] = str;
             }
-
-        TheEnd:
             return tmp;
         }
 
-        internal void addLabelToStore(string nowText, 
-                                        string[] nowLabelResultValues, 
+        internal void addLabelToStore(string nowText,
+                                        string[] nowLabelResultValues,
                                         string nowFilename)
         {
             int category;
 
             //nowLabelResultValues的元素个数 判断是否存在
-            if (nowLabelResultValues.Length == 3) 
+            if (nowLabelResultValues.Length == 3)
             {
                 category = 1;
             }
-            else if(nowLabelResultValues.Length == 4)
+            else if (nowLabelResultValues.Length == 4)
             {
                 category = Convert.ToInt16(nowLabelResultValues[3]);
             }
-            else 
+            else
             {
                 return;     //解析失败
             }
@@ -583,7 +652,7 @@ namespace LabelPlus
         {
             return addFile(nowFilename);
         }
-        #endregion
 
+        #endregion Methods
     }
 }
