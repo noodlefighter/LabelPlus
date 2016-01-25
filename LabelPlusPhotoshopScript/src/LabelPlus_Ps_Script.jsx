8b907934df80783a74099a99d33f86cd3a4e6285
noodlefighter <rongjiayu@gmail.com>
2016-01-26 01:05:23 +0800

    ps脚本 完善涂白动作 以及临时测试代码

--- LabelPlusPhotoshopScript/src/LabelPlus_Ps_Script.jsx
+++ LabelPlusPhotoshopScript/src/LabelPlus_Ps_Script.jsx
@@ -714,7 +714,28 @@ LabelPlusInput.prototype.process = function(opts, doc) {
         app.doAction("_start" , opts.runActionGroup);
       }
       catch(e){ }
-    }            
+    }  
+        
+    // 涂白测试开始 {
+    //todo: 删除这个测试段
+    var labelArr = new Array();
+    
+    // 找出需要涂白的标签
+    for(var j=0; j<labelData.length; j++){
+        var labelX = labelData[j].LabelheadValue[0];
+        var labelY = labelData[j].LabelheadValue[1];
+        var labelXY = { x:labelX, y:labelY };        
+        var labelGroup = gourpData[labelData[j].LabelheadValue[2]];
+        
+        if(labelGroup == "框内"){
+            labelArr.push(labelXY);
+        }            
+    }
+    
+    //执行涂白
+    MyAction.lp_dialogClear(labelArr, bg.width, bg.height, 16, 1);        
+    
+    // 涂白测试结束 }
     
     // 遍历LabelData
     for(var j=0; j<labelData.length; j++){
@@ -737,8 +758,8 @@ LabelPlusInput.prototype.process = function(opts, doc) {
         if(opts.outputLabelNumber && !layerGroups["_Label"]){
           layerGroups["_Label"] = bg.layerSets.add();
           layerGroups["_Label"].name = "Label";
-        }
-      
+        }        
+        
         // 导出标号
         if(opts.outputLabelNumber){
           LabelPlusInput.newTextLayer(bg,
